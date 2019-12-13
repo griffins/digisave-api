3c061d625c69c4f7e0c59c5dc850bb614f9d6b8a
Griffins Oringo <oringogriffins78@gmail.com>
2019-12-13 16:44:24 +0300

    first commit

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -36,10 +36,6 @@ class RouteServiceProvider extends ServiceProvider
     public function map()
     {
         $this->mapApiRoutes();
-
-        $this->mapWebRoutes();
-
-        //
     }
 
     /**
